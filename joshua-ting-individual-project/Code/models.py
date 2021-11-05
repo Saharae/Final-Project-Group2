64bf27ddeef09f73a1750736267bc0099333841a
justjoshtings <joshting1@gmail.com>
2021-11-04 21:28:07 -0400

    added individual parts

---
+++ joshua-ting-individual-project/Code/models.py
@@ -0,0 +1,13 @@
+# This file contains the models that we used for our dataset
+
+import pandas as pd
+import numpy as np
+from matplotlib import pyplot as plt
+import os
+from models_helper import Helper as hpr
+
+from sklearn.linear_model import LinearRegression
+
+
+
+    
---
+++ joshua-ting-individual-project/Code/models_helper.py
@@ -0,0 +1,12 @@
+# This file contains helper objects and functions for our models.py file.
+
+import pandas as pd
+import numpy as np
+from matplotlib import pyplot as plt
+import os
+
+from sklearn.linear_model import LinearRegression
+
+class Helper:
+    def __init__(self):
+        return
